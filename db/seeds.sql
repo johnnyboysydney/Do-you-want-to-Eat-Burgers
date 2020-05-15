1a5bfc6b2cfc7a282fdb46b1fe763c7e8f1cf322
Johnnyboysydney <54227198+johnnyboysydney@users.noreply.github.com>
2020-05-15 19:23:18 +1000

    Adding Seeds file

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+### Seeds 
+
+-- Insert rows into table
+USE burgers_db;
+INSERT INTO burgers (burger_name, devoured) 
+VALUES 
+('Cheese Burger', FALSE), 
+('Big Mac', FALSE),
+('Triple Chiken Burger', FALSE)
