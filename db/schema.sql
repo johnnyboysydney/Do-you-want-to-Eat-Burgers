84d40c99048d49a799f189326ec9a3c01f80e785
Johnnyboysydney <54227198+johnnyboysydney@users.noreply.github.com>
2020-05-20 19:06:13 +1000

    refactoring comments

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,4 @@
-### Schema
-
+-- Create the database and use it and then create a table
 DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 USE burgers_db;
@@ -7,7 +6,7 @@ USE burgers_db;
 CREATE TABLE burgers
 (
     id int NOT NULL AUTO_INCREMENT,
-    burger_name VARCHAR(255) NOY NULL,
+    burger_name VARCHAR(110) NOY NULL,
     devoured BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
 )
