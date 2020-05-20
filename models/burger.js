bf82f78fd2e2e92e81585f74181603ef82d84503
Johnnyboysydney <54227198+johnnyboysydney@users.noreply.github.com>
2020-05-20 19:12:07 +1000

    refactoring functions variable name and comments

---
+++ models/burger.js
@@ -0,0 +1,24 @@
+// Import the ORM to create functions that will interact with the database.
+const orm = require("../config/orm.js");
+
+let burger = {
+    all: function(cb) {
+      orm.SelectAll("burgers", function(res) {
+          cb(res);
+        });
+    },
+    // The variables cols and vals are arrays.
+    create: function(cols, vals, cb) {
+      orm.insertOne("burgers", cols, vals, function(res) {
+        cb(res);
+      });
+    },
+    update: function(objColVals, condition, cb) {
+      orm.updateOne("burgers", objColVals, condition, function(res) {
+        cb(res);
+      });
+    }
+};
+
+// Export the database functions for the controller (catsController.js).
+module.exports = burger;
