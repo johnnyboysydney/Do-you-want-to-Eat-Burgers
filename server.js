65c2ea10ab294808662f91da7e9457230a4fe7ca
Johnnyboysydney <54227198+johnnyboysydney@users.noreply.github.com>
2020-05-20 18:52:46 +1000

    Correcting code

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
 // Dependencies & Variables
 const express = require("express");
-const exphbs = require("express-handlebars")
+const exphbs = require("express-handlebars");
+const PORT = process.env.PORT || 9080; 
 
 // Initalise Express Server
 const app = express();
 
-let PORT = process.env.PORT || 9080; // Set Default Port for Express and Heroku
-app.use(bodyParser.urlencoded({ extended: false })); // Add Additional Functionality to Express Using Middleware body-parser
-app.use(express.static("public")); // Serve Static Content Such as CSS, JS, for the App from the "public" Directory in the Application Directory.
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(express.static("public")); 
 
+// Template engine
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+// Import routes
 const routes = require("./controllers/burgersController.js");
 app.use("/", routes);
 
-// Starts the server to begin listening
+// Starts listening
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
