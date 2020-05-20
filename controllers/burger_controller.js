bf270e190f7df867ad7accbc8c3515992061110f
Johnnyboysydney <54227198+johnnyboysydney@users.noreply.github.com>
2020-05-20 19:04:39 +1000

    refactoring code adding missing function 'put'

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -1,11 +1,9 @@
 // Dependencies and Variables
 let express = require("express");
-
 let router = express.Router();
 
 // Import the model to use its database functions.
 let burger = require("..models/burger.js")
-
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
@@ -17,6 +15,29 @@ router.get("/", function(req, res) {
       res.render("index", hbsObject);
     });
   });
-
+//post new burger
+router.post("/api/burgers", function(req, res) {
+  burger.create(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
+    //send back the ID of the new burger
+    res.json({ id: result.insertId });
+  });
+});
+//put updated burger
+router.put("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+  burger.update(
+    {
+      devoured: req.body.devoured
+    },
+    condition,
+    function(result) {
+      if (result.changedRows === 0) {
+        //return 404 if no rows were changed, 
+        return res.status(404).end();
+      }
+      res.status(200).end();
+    }
+  );
+});
  // Export routes for server.js to use.
 module.exports = router; 
