d45eab9aa1a8d5712c1e83fde385c8d6d1f67cbb
Johnnyboysydney <54227198+johnnyboysydney@users.noreply.github.com>
2020-05-20 20:26:04 +1000

    Added heroku DB connector

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 // Set up MySQL connection.
 let mysql = require("mysql");
 
-let connection = mysql.createConnection({
+let connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "Terror301",
     database: "burgers_db"
 });
-
+}
 // Make connection.
 connection.connect(function(err) {
     if (err) {
