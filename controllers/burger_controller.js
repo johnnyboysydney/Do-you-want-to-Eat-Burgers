e5ec33cce3ec46051d9d9a1988bb1912457db18c
Johnnyboysydney <54227198+johnnyboysydney@users.noreply.github.com>
2020-05-20 19:27:42 +1000

    added missing / in the require statement

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -3,7 +3,7 @@ let express = require("express");
 let router = express.Router();
 
 // Import the model to use its database functions.
-let burger = require("..models/burger.js")
+let burger = require("../models/burger.js")
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
