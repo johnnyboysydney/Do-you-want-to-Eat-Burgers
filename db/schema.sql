3776158273ff1e54944a34741a337071c062e1d5
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-05-23 18:17:52 +1000

    finsihed

--- db/schema.sql
+++ db/schema.sql
@@ -6,7 +6,7 @@ USE burgers_db;
 CREATE TABLE burgers
 (
     id int NOT NULL AUTO_INCREMENT,
-    burger_name VARCHAR(110) NOY NULL,
+    burger_name VARCHAR(110) NOT NULL,
     devoured BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
 )
